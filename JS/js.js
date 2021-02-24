bd63267730ebed351d64a537264a360bbb9fdda0
felipemininel <felipe351@gmail.com>
2021-02-24 17:18:22 -0300

    Primeiro Commit

---
+++ JS/js.js
@@ -0,0 +1,14 @@
+var nome = "Felipe";
+var idade = 23;
+var pretensaoSalarial = 3277.22
+var valeRefeicao = true
+
+function teste1(){
+    console.log("foi pro teste 1")
+}
+
+
+function apertaOBotao(){
+window.alert("Eite carai")
+
+}
